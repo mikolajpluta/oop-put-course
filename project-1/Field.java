5b56db00083410078593c5bf3a87f9299f017ec9
mikolajpluta <63498420+mikolajpluta@users.noreply.github.com>
2022-12-03 13:27:05 +0100

    field class completed and first images added

--- project-1/Field.java
+++ project-1/Field.java
@@ -10,24 +10,62 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.util.List;
 
 public class Field implements EventHandler<ActionEvent> {
 
-    Button bt;
-    private Image img;
-    private ImageView field;
-    private int[] position;
-    private Boolean hidden;
+    private Button bt = new Button();
+    private String img_src;
+    private String hidden_img_src = "D:\\school\\sem_III\\Programowanie_obiektowe\\oop-put-course\\project-1\\memorygame\\src\\Main\\img\\Banana-Single.jpg";
+    private Image img = new Image(this.hidden_img_src);
+    private BackgroundSize s = new BackgroundSize(200, 200, false, false, true, true);
+    private BackgroundImage bg_img = new BackgroundImage(this.img, null, null, null, this.s);
+    private Background bg = new Background(this.bg_img);
+    private Boolean isVisible = false;
+    private Boolean isDone = false;
+
+    private String clas;
+    private int id;
+
+    private void show(){
+        this.img = new Image(this.img_src);
+        this.isVisible = true;
+    }
+    private void hide(){
+        this.img = new Image(this.hidden_img_src);
+        this.isVisible = false;
+    }
+    private void update(){
+        this.bg_img = new BackgroundImage(this.img, null, null, null, this.s);
+        this.bg = new Background(this.bg_img);
+        this.bt.setBackground(this.bg);
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.out.println("tez dziala");
+        if(!isDone) {
+            if(!isVisible) {
+                this.show();
+            }else{
+                this.hide();
+            }
+            this.update();
+        }
     }
-    public Field(String text){
+    public Field(GridPane p,String cls, int i, int pos_x, int pos_y, String src){
         this.bt.setOnAction(this);
-        this.bt = new Button();
+        this.bt.setBackground(this.bg);
+        this.bt.setPrefSize(200, 200);
+        GridPane.setConstraints(this.bt, pos_x, pos_y);
+        p.getChildren().addAll(this.bt);
+        this.img_src = src;
+        this.clas = cls;
+        this.id = i;
     }
 }
--- project-1/Main.java
+++ project-1/Main.java
@@ -25,43 +25,20 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
+        //window setup
         window = stage;
         window.setTitle("memory game");
-
         grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(10);
         grid.setHgap(10);
 
-
-        Image img = new Image("E:\\project-1\\memorygame\\src\\Main\\img\\Banana-Single.jpg");
-        ImageView view = new ImageView(img);
-        Image img2 = new Image("E:\\project-1\\memorygame\\src\\Main\\img\\Banana-Single.jpg");
-        ImageView view2 = new ImageView(img);
-        //Setting the position of the image
-
-        BackgroundSize s = new BackgroundSize(500,500, false, false, true, true);
-        BackgroundImage bi = new BackgroundImage(img, null, null, null, s);
+        
 
 
-        Background b = new Background(bi);
-
-        Button bt = new Button();
-        bt.setBackground(b);
-        bt.setPrefSize(500, 500);
-
-        GridPane.setConstraints(bt, 0, 0);
-
-        System.out.println(bi.getSize());
-
-        grid.getChildren().addAll(bt);
-
-
+        //window display
         Scene scene = new Scene(grid, 1000, 1000);
-
         window.setScene(scene);
-
-
         window.show();
 
 
