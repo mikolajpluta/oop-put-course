4ab6ec5498445bed571e48a5d07cf8fbeae8b205
mikolajpluta <63498420+mikolajpluta@users.noreply.github.com>
2022-12-04 23:07:55 +0100

    event handling changed, database class removed
    game prototype is ready, everythisg is working, game refreshes after every click so there can occour some "empty" clicks,
    working on making delay after bad choose of two fields and overlay.

--- project-1/Field.java
+++ project-1/Field.java
@@ -14,9 +14,9 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import java.util.List;
 
-public class Field implements EventHandler<ActionEvent> {
+public class Field{
 
-    private Button bt = new Button();
+    public Button bt = new Button();
     private String img_src;
     private String hidden_img_src = "D:\\school\\sem_III\\Programowanie_obiektowe\\oop-put-course\\project-1\\memorygame\\src\\Main\\img\\Banana-Single.jpg";
     private Image img = new Image(this.hidden_img_src);
@@ -29,37 +29,29 @@ public class Field implements EventHandler<ActionEvent> {
     private String clas;
     private int id;
 
-    private void show(){
-        this.img = new Image(this.img_src);
-        this.isVisible = true;
-    }
-    private void hide(){
-        this.img = new Image(this.hidden_img_src);
-        this.isVisible = false;
-    }
-    private void update(){
+    public void update(){
         this.bg_img = new BackgroundImage(this.img, null, null, null, this.s);
         this.bg = new Background(this.bg_img);
         this.bt.setBackground(this.bg);
     }
-
-    public void markAsDone(){
-        this.isDone = true;
+    public void show(){
+        this.img = new Image(this.img_src);
+        this.isVisible = true;
+        this.update();
+    }
+    public void hide(){
+        this.img = new Image(this.hidden_img_src);
+        this.isVisible = false;
+        this.update();
     }
 
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        if(!isDone) {
-            if(!isVisible) {
-                this.show();
-            }else{
-                this.hide();
-            }
-            this.update();
-        }
-    }
+    public void markAsDone(){this.isDone = true;}
+    public Boolean checkIfDone(){return this.isDone;}
+    public void markAsVisible(){this.isVisible = true;}
+    public Boolean checkIfVisible(){return this.isVisible;}
+    public String clas(){return this.clas;}
+
     public Field(GridPane p,String cls, int i, int pos_x, int pos_y, String src){
-        this.bt.setOnAction(this);
         this.bt.setBackground(this.bg);
         this.bt.setPrefSize(200, 200);
         GridPane.setConstraints(this.bt, pos_x, pos_y);
--- project-1/Main.java
+++ project-1/Main.java
@@ -1,6 +1,9 @@
 package Main;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,18 +13,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class Main extends Application{
+public class Main extends Application implements EventHandler<ActionEvent>{
 
      public Stage window;
      public GridPane grid;
+     public List<Field> visible = new ArrayList<Field>();
+     public int done = 0;
+     public Field[] fields = new Field[16];
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    public List<String[]> data = new ArrayList<String[]>();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -32,18 +44,74 @@ public class Main extends Application{
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(10);
         grid.setHgap(10);
-
-        
-
-
-        //window display
         Scene scene = new Scene(grid, 1000, 1000);
         window.setScene(scene);
         window.show();
 
+        //storing fields data
+        for(int i=0; i<2; i++) {
+            data.add(new String[]{"one", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img1.png"});
+            data.add(new String[]{"two", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img2.png"});
+            data.add(new String[]{"three", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img3.png"});
+            data.add(new String[]{"four", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img4.png"});
+            data.add(new String[]{"five", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img5.png"});
+            data.add(new String[]{"six", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img6.png"});
+            data.add(new String[]{"seven", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img7.png"});
+            data.add(new String[]{"eight", "D:/school/sem_III/Programowanie_obiektowe/oop-put-course/project-1/memorygame/src/Main/img/img8.png"});
+        }
+        int nr = 0;
+        //shuffling fields
+        Collections.shuffle(data);
+        //generating new fields
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                fields[nr] = new Field(grid, data.get(nr)[0], nr, i, j, data.get(nr)[1]);
+                fields[nr].bt.setOnAction(this);
+                nr++;
+            }
+        }
+    }
 
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        for(int i=0; i<16; i++){
+            if(actionEvent.getSource()==this.fields[i].bt){
 
-
-
+                //game logic
+                if(this.done < 16){
+                    if(this.visible.size() < 2){
+                        if(!this.fields[i].checkIfDone()){
+                            if(!this.fields[i].checkIfVisible()){
+                                this.fields[i].show();
+                                this.visible.add(fields[i]);
+                            }
+                        }
+                    }else{
+                        if(this.visible.get(0).clas() == this.visible.get(1).clas()){
+                            this.visible.get(0).markAsDone();
+                            this.visible.get(1).markAsDone();
+                            this.done += 2;
+                        }else{
+                            this.visible.get(0).hide();
+                            this.visible.get(1).hide();
+                        }
+                        this.visible = new ArrayList<Field>();
+                    }
+                }else{
+                    int nr = 0;
+                    //shuffling fields
+                    Collections.shuffle(this.data);
+                    //generating new fields
+                    for(int k=0; k<4; k++){
+                        for(int j=0; j<4; j++){
+                            fields[nr] = new Field(grid, data.get(nr)[0], nr, k, j, data.get(nr)[1]);
+                            fields[nr].bt.setOnAction(this);
+                            nr++;
+                        }
+                    this.done = 0;
+                    }
+                }
+            }
+        }
     }
 }
